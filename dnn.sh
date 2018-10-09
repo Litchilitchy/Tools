0278f427ed2c0f7cdf9e14d82b38c5455999cece
SummerLitchichichy <657911141@qq.com>
2018-10-09 17:06:55 +0800

    Create dnn.sh

---
+++ dnn.sh
@@ -0,0 +1,9 @@
+# first cuda 
+# https://developer.nvidia.com/cuda-92-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1604&target_type=debnetwork
+
+# mxnet installation 
+# https://mxnet.incubator.apache.org/install/ubuntu_setup.html#cuda-dependencies
+
+# then install other py libs
+pip3 install opencv-python      # cv2
+pip3 install Pillow             # PIL
